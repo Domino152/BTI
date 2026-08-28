69a9ab76a9d81eb82084293e62246eda3b419eae
backup-alt <universeexplorer4@gmail.com>
2026-08-28 22:11:55 +0530

    Fix GitHub Pages routing with basename

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/V2NTA">
       <AppProvider><App /></AppProvider>
     </BrowserRouter>
   </React.StrictMode>,
